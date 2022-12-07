a1ce44bf0d9fe3d680f9498c224a6050a273f9e1
lriley2020 <liam@liamriley.me>
2022-12-07 18:34:18 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,21 @@
+elves = []
+
+def addUp(*meals):
+    return sum(*meals)
+
+with open("day1input.txt", "r") as f:
+    newelf = []
+    for line in f.readlines():
+        if len(line.strip()) == 0:
+            elves.append(newelf)
+            newelf = []
+        else:
+            newelf.append(int(line.strip()))
+
+elvestotal = list(map(addUp, elves))
+print(max(elvestotal))
+
+### Part 2 ###
+
+elvestotal.sort(reverse=True)
+print(sum(elvestotal[:3]))
