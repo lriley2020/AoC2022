223d0defcbd25aaaa9bbb9071f6129f8bfaffbe1
lriley2020 <liam@liamriley.me>
2022-12-08 17:29:49 +0000

    Day 4

---
+++ day3.py
@@ -0,0 +1,26 @@
+from string import ascii_lowercase, ascii_uppercase
+priorities = list(ascii_lowercase + ascii_uppercase)
+
+with open("day3input.txt", "r") as f:
+    bags = f.read().splitlines()
+
+def addUp(lis):
+    total = 0
+    for li in lis:
+        total += (priorities.index(li) + 1)
+    return total
+
+intersections = []
+for bag in bags:
+    sec1, sec2 = bag[:len(bag) // 2], bag[len(bag) // 2:]
+    intersections += list(set(sec1).intersection(set(sec2)))[0]
+
+print(addUp(intersections))
+
+## part 2 ##
+intersections2 = []
+for i in range(0, len(bags), 3):
+    b1, b2, b3 = bags[i:i + 3]
+    intersections2 += list((set(b1).intersection(set(b2), set(b3))))[0]
+
+print(addUp(intersections2))
---
+++ day4.py
@@ -0,0 +1,29 @@
+with open("day4input.txt", "r") as f:
+    theinput = f.read().splitlines()
+
+pairs = []
+for line in theinput:
+    pair = line.split(",")
+    parsedpair = []
+    for elf in pair:
+        parsedpair.append(tuple((map(int, elf.split("-")))))
+    pairs.append(tuple(parsedpair))
+
+count = 0
+for pair in pairs:
+    elf1range = range(pair[0][0], pair[0][1] + 1)
+    elf2range = range(pair[1][0], pair[1][1] + 1)
+    if all(e in elf1range for e in elf2range) or all(e in elf2range for e in elf1range):
+        count += 1
+
+print(count)
+
+## Part 2 ##
+count2 = 0
+for pair in pairs:
+    elf1range = range(pair[0][0], pair[0][1] + 1)
+    elf2range = range(pair[1][0], pair[1][1] + 1)
+    if set(elf1range).intersection(set(elf2range)):
+        count2 += 1
+
+print(count2)
