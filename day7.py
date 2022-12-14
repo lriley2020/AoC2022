a4897cecfc04ed01a5b8f6af2b4c223ef33ced58
lriley2020 <liam@liamriley.me>
2022-12-14 15:12:45 +0000

    Working scraper for day 7

--- day7.py
+++ day7.py
@@ -7,44 +7,60 @@ dirtotals = {}
 ## eg: [("a","b","c"), ("x", "y", "z")]
 subdirs = []
 
-
 def addUp():
-    subdirs[currdir] = []
+    print(f"Adding up directory {currdir}")
+    subs = [currdir]
     total = 0
+    print(lslines)
     for line in lslines:
         if "dir" in line:
-            subdirs[currdir].append(line[4:])
+            subs.append(line[4:])
         else:
             size = ""
             for char in line:
                 if char.isnumeric():
                     size += char
             total += int(size)
+    print(f"About to commit, input line number {linenum}, total for this directory is {total}")
+    print()
     dirtotals[currdir] = total
+    subdirs.append(tuple(subs))
 
 
 lslines = []
 ## first dir should be root, /
-currdir = theinput[0][5:]
+currdir = "/"
+linenum = 0
 for line in theinput:
+    linenum += 1
+    #print(f"Input line number {linenum}, current directory is {currdir}")
+    ## Is this a command? ##
     if "$" in line:
         if "cd" in line:
-            ## If we are going back 1 level
+            if lookForEndOfLs:
+                addUp()
+                lookForEndOfLs = False
             if ".." in line:
+                print(f"Going backwards, input line number {linenum}")
                 ## Find the directory containing the current directory ##
                 for dir in subdirs:
                     if currdir in dir[1:]:
-                        prevdir: str = dir[0]
+                        prevdir = dir[0]
                 ## Change the current directory to the previous one ###
+                print(f"The previous directory to {currdir} is {prevdir}")
                 currdir = prevdir
-            addUp()
-            currdir = line[5:]
-            lslines = []
+            else:
+                ## If we are not changing to an outer directory, change to the next one ##
+                currdir = line[5:]
         elif "ls" in line:
-            continue
+            lookForEndOfLs = True
+            lslines = []
     else:
         ## If this is the output from an ls command, add it too the list
         lslines.append(line)
+addUp()
 
+print()
 print(dirtotals)
+print()
 print(subdirs)
